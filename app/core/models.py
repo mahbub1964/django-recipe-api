a1147593d3a3973cda6b34eb5dcf788d2efdb2c5
Syed Mahbubur Rahman <mahbubur.rahman.64@gmail.com>
2022-07-11 06:14:41 +0600

    Create a recipe image upload API

--- app/core/models.py
+++ app/core/models.py
@@ -6,6 +6,15 @@ from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser, BaseUserManager, PermissionsMixin
 )
+import os
+import uuid
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image."""
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+    return os.path.join('uploads', 'recipe', filename)
 
 
 class UserManager(BaseUserManager):
@@ -51,6 +60,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
     ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
